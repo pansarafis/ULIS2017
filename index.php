dd76fe1580f135de0d47a697c7460db602392e38
pansarafis <pansarafis@yahoo.gr>
2016-07-03 21:11:59 +0300

    Bootstrapping and rolling

---
+++ index.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>EUROSOI - ULIS 2017</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/favicon.ico">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style2.css" >
+    <link rel="stylesheet" href="css/carousel_fade.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<!--    <script src="http://cdn.jsdelivr.net/modernizr/2.8.3/modernizr.min.js"></script>-->
+<!--    <script type="text/javascript" src="js/jssor.slider.min.js"></script>-->
+</head>
+<body>
+
+<div class="container">
+    
+    
+<?php //include 'header.php'?>
+
+<div class="headerUp">
+    <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+       <!--Indicators--> 
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
+
+       <!--Wrapper for slides--> 
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img src="img/ulis01.jpg" alt="Chania">
+        </div>
+
+        <div class="item">
+          <img src="img/ulis02.jpg" alt="Chania">
+        </div>
+
+        <div class="item">
+          <img src="img/ulis03.jpg" alt="Flower">
+        </div>
+
+        <div class="item">
+          <img src="img/ulis04.jpg" alt="Flower">
+        </div>
+      </div>
+
+       <!--Left and right controls--> 
+<!--      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>-->
+      </a>
+    </div>
+
+    <div class="title">
+        <div class="logo">
+            <img src="images/logo_eurosoi_ulis-trans.png">
+        </div>
+        <div class="headings">
+            <h1>
+              <a id="menubutton" href="index.html">EUROSOI-ULIS 2017</a>
+            </h1>
+        </div>
+    </div>
+
+</div>
+    
+<div class="subtitles  title">
+    <h2>
+        2017 Joint International EUROSOI Workshop and
+        International Conference on Ultimate Integration on Silicon
+    </h2>
+    <h3>
+        April 25-27, 2017 - Athens, Greece
+    </h3>
+</div>
+
+    
+<nav class="navbar navbar-inverse">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span> 
+        </button>
+        <a class="navbar-brand" href="#"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+            <li class=""><a href="#">Home</a></li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Information<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Call for Papers</a></li>
+                    <li><a href="">Important Dates</a></li>
+                    <li><a href="">Plenary Talks</a></li>
+                    <li><a href="#">Conference Program</a></li>
+                    <li><a href="">Social events</a></li>
+                    <li><a href="">Committee</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Participate<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li class="col-"><a href="#">Paper Submission</a></li>
+                    <li class="col-"><a href="#">Registration</a></li>
+                    <li class="col-"><a href="#">SINANO Award</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Venue<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li class="col-"><a href="#">Venue</a></li>
+                    <li class="col-"><a href="#">Accomodation</a></li>
+                    <li class="col-"><a href="#">Travel</a></li>
+                    <li class="col-"><a href="#">About Athens</a></li>
+                </ul>
+            </li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+</nav>
+    
+    <div class="main max">
+        
+        <div class="chipImg">
+            <img src="images/chipgrey.png" />
+	</div>
+        <div class="mainContainer">
+            <h2>Scope of the Conference</h2>
+            <p>After the great success of the first EUROSOI-ULIS event in Bologna, it is our pleasure to announce that the second joint EUROSOI-ULIS event will be hosted by the Institute for Microelectronics, TU Wien. The focus of the sessions lies on SOI technology and advanced nanoscale devices. The organizing committee invites to actively participate by submitting high quality papers.</p>
+           
+            <h2>Call for Papers</h2> 
+            <p>Second-call-for-papers</p>
+            
+            <h2>Important Dates</h2>
+            <p>The conference starts on January 25 at 9.30 with the registration and coffee.</p>
+
+            <h2>Topics include, but are not limited to :</h2>
+            <div class="lista">
+                <ul>
+                    <li> Advanced SOI materials and wafers </li>
+                    <li> New channel materials for CMOS: strained Si, strained SOI, SiGe, GeOI, III-V and high mobility materials on insulator; carbon nanotubes; graphene and other 2D materials </li>
+                    <li> Properties of ultra-thin films and buried oxides, defects, interface quality </li>
+                    <li> Thin gate dielectrics: high-k materials for switches and memory </li>
+                    <li> Nanometer scale devices: technology, characterization techniques and evaluation metrics for high performance, low power, low standby power, high frequency and memory (DRAM, Non-Volatile, SRAM, ReRAM...) applications</li>
+                    <li> Physics mechanisms and innovative SOI-like devices</li>
+                    <li> Alternative transistor architectures including FDSOI, DGSOI, FinFET, MuGFET, vertical MOSFET, Nanowires, FeFET and tunnel FET structures, MEMS/NEMS, Beyond-CMOS nanodevices </li>
+                    <li> New functionalities in silicon-compatible nanostructures and innovative devices representing the More than Moore domain for Sensing, Energy harvesting, RF, High voltage, Imagers, Electronic cooling, etc.</li>
+                    <li> CMOS scaling perspectives; device/circuit level performance evaluation; switches and memory scaling</li>
+                    <li> Three-dimensional integration of devices and circuits, heterogeneous integration</li>
+                    <li> Transport phenomena, compact modeling, device simulation, front- and back-end fabrication process simulation</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li><li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    <li> Advanced test structures and characterization techniques, parameter extraction, reliability and variability issues for new materials and novel devices</li>
+                    
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+<div class="footer max">
+	<div class="footerlogos">
+		<img src="images/demokritos.png">
+	</div>
+	<div class="footerlogos">
+		<img src="images/sinano_trans.png">
+	</div>
+	<div class="footerlogos">
+		<img src="images/logo_eds.png">
+	</div>
+	<div class="footerlogos">
+		<img src="images/IEEE_logo.png">
+	</div>
+</div>
+</div>
+
+</body>
+</html>
